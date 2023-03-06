cb09a0b04d0debeed3736970e1fcb8da0c19f05d
JBDEV\jacbr <jacbrob@gmail.com>
2023-03-06 16:47:00 +0100

    Sample app

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public partial class MonkeyService
+{
+    HttpClient httpClient;
+    List<Monkey> monkeyList = new();
+
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+
+    public async Task<List<Monkey>> GetMonkeysAsync()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        var response = await httpClient.GetAsync("https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/MonkeysApp/monkeydata.json");
+        if (response.IsSuccessStatusCode)
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+
+        return monkeyList;
+    }
+}
---
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,122 @@
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+    private readonly MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+    private readonly IConnectivity connectivity;
+    private readonly IGeolocation geolocation;
+
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
+    }
+
+    [ObservableProperty]
+    bool isRefreshing;
+
+    [RelayCommand]
+    public async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count == 0)
+            return;
+
+        try
+        {
+            var location = await geolocation.GetLocationAsync();
+            if (location is null)
+            {
+                location = await geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30),
+                    });
+            }
+
+            if (location is null)
+                return;
+
+            var first = Monkeys
+                .OrderBy(m => 
+                    location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles))
+                .FirstOrDefault();
+
+            if (first is null)
+                return;
+
+            await Shell.Current.DisplayAlert("Closest Monkey",
+                $"{first.Name} in {first.Location}", "OK");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert(
+                "Error!", $"Unable to get closest monkey: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+    
+    [RelayCommand]
+    public async Task GoToDetailsAsync(Monkey monkey)
+    {
+        if (monkey is null)
+            return;
+
+        await Shell.Current.GoToAsync($"{nameof(DetailsPage)}", true,
+            new Dictionary<string, object>
+            {
+                { "Monkey", monkey }
+            });
+    }
+
+
+    [RelayCommand]
+    public async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert(
+                                       "No Internet", 
+                                       "You must be connected to the internet to get monkeys", 
+                                       "OK");
+                return;
+            }
+
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeysAsync();
+
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert(
+                "Error!", $"Unable to get monkeys: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+            IsRefreshing = false;
+        }
+    }
+}
